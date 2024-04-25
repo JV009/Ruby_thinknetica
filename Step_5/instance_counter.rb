4159ae01e4470f35d5496ebee16421c19c1001a4
JV009 <krysov_aleksandr@mail.ru>
2024-04-25 10:08:09 +0300

    Step_5

--- Step_5/instance_counter.rb
+++ Step_5/instance_counter.rb
@@ -8,11 +8,11 @@ module InstanceCounter
     attr_accessor :instances
 
     def instances
-      @instances = 0
+      @instances ||= 0
     end
 
     def register_instance
-      @instances = 0
+      @instances ||= 0
       @instances += 1
     end
   end
